ab58eba82e0339e98f94ee8bf98733540517a602
Ashwin Gadve <gadveashwin@gmail.com>
2024-11-04 11:04:29 +0530

    Initial commit

---
+++ ECommerceWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ECommerceWebMVC.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
---
+++ ECommerceWebMVC/Views/Home/Ashwin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ECommerceWebMVC.Views.Home
+{
+    public class AshwinModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
